b5222b37d4f106a2560b1da44c8a8433c42f7892
[yewonb] <satoo3@naver.com>
2025-06-05 01:49:47 +0900

    .

--- modelproject/settings.py
+++ modelproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '52.64.190.215', 'yxddxng.store']
 
 
 # Application definition
